dadd46df0ca8e47b17995cd4f870f43e8d0b2018
farmasek <vojtabaranek@gmail.com>
2019-03-25 15:40:00 +0100

    Updates

---
+++ precache-manifest.bb13afcb89b6381c5c4b6df737394ce9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d465afae46d3f87a8412",
+    "url": "/zonky-fe-test/static/js/runtime~main.e7eb5732.js"
+  },
+  {
+    "revision": "9f909e6fb82d62589b67",
+    "url": "/zonky-fe-test/static/js/main.659ef07b.chunk.js"
+  },
+  {
+    "revision": "28f8e9957739c9efd6db",
+    "url": "/zonky-fe-test/static/js/2.27dd99fa.chunk.js"
+  },
+  {
+    "revision": "9f909e6fb82d62589b67",
+    "url": "/zonky-fe-test/static/css/main.4a3772a2.chunk.css"
+  },
+  {
+    "revision": "f58ee3772b4ae14b2731654f028083c2",
+    "url": "/zonky-fe-test/index.html"
+  }
+];
